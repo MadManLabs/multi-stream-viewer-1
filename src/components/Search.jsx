a66faa759b3dcf1339344deec84215a2b83ae8ca
Omari Mayers-Walker <omari@fastmail.fm>
2016-03-09 19:16:45 -0500

    created needed components files

---
+++ src/components/Search.jsx
@@ -0,0 +1,13 @@
+'use strict';
+
+var React = require('react');
+
+var Search = React.createClass({
+  render: function() {
+    return (
+      <h3>Search</h3>
+    );
+  }
+});
+
+module.exports = Search;
--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -1,6 +1,18 @@
+'use strict';
+
 var React = require('react');
+var Search = require('./Search.jsx');
 
 var Viewer = React.createClass({
+  render: function() {
+    return (
+      <div>
+      <h1>Multi Stream Viewer</h1>
+      <Search/>
+      </div>
+
+    );
+  }
 });
 
 module.exports = Viewer;
